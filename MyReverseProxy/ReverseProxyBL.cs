60d8db3d22ecb7d2b6e34763068b2b62f86fc305
budix669 <budix669@gmail.com>
2022-02-17 12:38:27 +0200

    lalala

--- MyReverseProxy/ReverseProxyBL.cs
+++ MyReverseProxy/ReverseProxyBL.cs
@@ -1,4 +1,7 @@
-﻿namespace MyReverseProxy
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace MyReverseProxy
 {
     public static class ReverseProxyBL
     {
@@ -8,7 +11,7 @@
 
         internal static string GenerateKey(string path)
         {
-            string key = counter.ToString();
+            string key = createHash(path);
 
             checkCleanup();
             
@@ -16,6 +19,54 @@
             counter++;
             return key;
             
+        }
+
+        private static string createHash(string source)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                string hash = GetHash(sha256Hash, source);
+
+                if (VerifyHash(sha256Hash, source, hash))
+                {
+            
+                }
+                else
+                {
+                }
+                return hash;
+            }
+        }
+        // Verify a hash against a string.
+        private static bool VerifyHash(HashAlgorithm hashAlgorithm, string input, string hash)
+        {
+            // Hash the input.
+            var hashOfInput = GetHash(hashAlgorithm, input);
+
+            // Create a StringComparer an compare the hashes.
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            return comparer.Compare(hashOfInput, hash) == 0;
+        }
+        private static string GetHash(HashAlgorithm hashAlgorithm, string input)
+        {
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            var sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
         }
 
         private static void checkCleanup()
@@ -26,4 +77,6 @@
             }
         }
     }
+
+    
 }
